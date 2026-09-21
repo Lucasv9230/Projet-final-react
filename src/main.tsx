3ebc3321dd7e365c1ee6c43cfee2fe1b9d3eaaac
Lucas <lucas.vauclin@ynov.com>
2026-09-21 11:27:50 +0200

    add new import

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import RoutesFile from './RoutesFile'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import RoutesFile from "./RoutesFile";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <RoutesFile />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
