ce6024ba724372178558c843f14d88a7fe3e7209
Lucas <lucas.vauclin@ynov.com>
2026-09-21 10:22:08 +0200

    add route file in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import RoutesFile from './RoutesFile'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <RoutesFile />
+    </BrowserRouter>
   </StrictMode>,
 )
